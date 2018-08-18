30859794bf24cc726eb3f11282f596629306a07c
JayLee07 <wpgur0107@gmail.com>
2018-08-18 19:37:20 +0900

    modified networks.py

--- networks.py
+++ networks.py
@@ -2,34 +2,41 @@ import torch
 import torch.nn as nn
 import numpy as np
 from torch.autograd import Variable
-from model_utils import *
+from torch.optim import lr_scheduler
+from model_utils import get_norm_layer, initialize_weights
 
-def define_G(ch_in, ch_out, ngf, model_type, norm_type='batch', use_dropout=False, init_type='normal', gpu_ids=[]):
-    norm_layer = get_norm_layer(norm_type)
-    if model_type == 'resnet_9blocks':
-        netG = ResnetG(ch_in, ch_out, ngf, norm_layer, use_dropout, 9)
-    elif model_type == 'resnet_6blocks':
-        netG = ResnetG(ch_in, ch_out, ngf, norm_layer, use_dropout, 6)
-    else:
+def generator(args, gpu_ids=[]):
+    norm_layer = get_norm_layer(args.norm_type)
+    netG = ResnetG(args, gpu_ids=gpu_ids)
+    if args.modelG != 'resnet_9blocks' and args.modelG != 'resnet_6blocks':
         raise NotImplementedError('Enter the proper name of G')
     if len(gpu_ids) > 0:
         netG = netG.cuda(gpu_ids[0])
     netG.apply(initialize_weights)
     return netG
 
-def define_D(ch_in, ndf, model_type, n_layers=3, norm_type='batch', use_sigmoid=False, init_type='normal', gpu_ids=[]):
-    norm_layer = get_norm_layer(norm_type)
-    if model_type == 'n_layer':
-        netD = NLayerD(ch_in, ndf, n_layers, norm_layer, use_sigmoid, gpu_ids)
-    elif model_type == 'pixel':
-        netD = PixelD(ch_in, ndf, norm_layer, use_sigmoid, gpu_ids)
+
+def discriminator(args, gpu_ids=[]):
+    norm_layer = get_norm_layer(args.norm_type)
+    if args.modelD == 'n_layer':
+        netD = NLayerD(args, gpu_ids=gpu_ids, n_layers=3)
+    elif args.modelD == 'pixel':
+        netD = PixelD(args, gpu_ids)
     else:
-        raise NotImplementedError('Enter the proper name of D')
+        raise NotImplementedError('{} is not an appropriate modelG name'.format(args.modelD))
     if len(gpu_ids) > 0:
         netD = netD.cuda(gpu_ids[0])
     netD.apply(initialize_weights)
     return netD
 
+def scheduler_lr(args, optimizer):
+    if args.lr_policy == 'step':
+        scheduler = lr_scheduler.StepLR(optimizer, step_size = args.lr_decay_iters, gamma=0.1)
+    elif args.lr_policy == 'plateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
+    else:
+        return NotImplementedError('{} is not the proper name of scheduler_lr'.format(args.lr_policy))
+    return scheduler
 
 class ResnetBlock(nn.Module):
     def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias):
@@ -38,8 +45,8 @@ class ResnetBlock(nn.Module):
     
     def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
         conv_block = []
-        
         padding_size=0
+        
         if padding_type == 'reflect':
             conv_block += [nn.ReflectionPad2d(1)]
         elif padding_type == 'replicate':
@@ -47,9 +54,16 @@ class ResnetBlock(nn.Module):
         elif padding_type == 'zero':
             padding_size = 1
         else:
-            raise NotImplementedError('pading {} is not implemented'.format(padding_type))
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=padding_size, bias=use_bias), 
-                       norm_layer(dim), nn.ReLU(True)]
+            raise NotImplementedError('padding {} is not implemented'.format(padding_type))
+        
+        conv_layer = nn.Conv2d(in_channels = dim,
+                               out_channels = dim,
+                               kernel_size=3,
+                               padding=padding_size,
+                               bias=use_bias)
+        conv_block += [conv_layer, 
+                       norm_layer(dim), 
+                       nn.ReLU(True)]
         if use_dropout == True:
             conv_block += [nn.Dropout(0.5)]
         
@@ -62,99 +76,188 @@ class ResnetBlock(nn.Module):
             padding_size = 1
         else:
             raise NotImplementedError('pading {} is not implemented'.format(padding_type))
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=padding_size, bias=use_bias), norm_layer(dim)]
+        
+        conv_block += [conv_layer, norm_layer(dim)]
         return nn.Sequential(*conv_block)
     
     def forward(self, x):
-        out = x+ self.conv_block(x)
+        out = x + self.conv_block(x)
         return out
     
     
 class ResnetG(nn.Module):
-    def __init__(self, ch_in, ch_out, ngf = 64, norm_layer = nn.BatchNorm2d, use_dropout=False, n_blocks = 6, padding_type = 'reflect', gpu_ids = []):
-        assert(n_blocks>=0)
+    def __init__(self, args, gpu_ids=[]):
         super(ResnetG, self).__init__()
-        self.ch_in = ch_in
-        self.ch_out = ch_out
-        self.ngf = ngf
+        self.args = args
         self.gpu_ids = gpu_ids
+        norm_layer = get_norm_layer(self.args.norm_type)
+        
         if norm_layer == nn.InstanceNorm2d:
             use_bias = True
         else:
             use_bias = False
-        model = [nn.ReflectionPad2d(3), nn.Conv2d(ch_in, ngf, kernel_size=7, padding=0,bias=use_bias), norm_layer(ngf, affine=True), nn.ReLU(True)]
         
-        n_downsampling=2
+        if args.modelG == 'resnet_9blocks':
+            n_blocks=9
+        elif args.model_G == 'resnet_6blocks':
+            n_blocks=6
+        else:
+            raise NotImplementedError('{} is not an appropriate modelG name'.format(args.modelG))
+        assert(n_blocks>=0)
+        
+        #Construct a model
+        n_downsampling = 2
+        mult = 2 ** n_downsampling
+        
+        # First Module
+        conv_layer = nn.Conv2d(in_channels = args.ch_in,
+                               out_channels = args.ngf,
+                               kernel_size=7,
+                               padding=0,
+                               bias=use_bias)
+        layers = [nn.ReflectionPad2d(3), 
+                  conv_layer, 
+                  norm_layer(args.ngf, affine=True),
+                  nn.ReLU(True)]
+        
+        # Downsamples with conv layers
         for i in range(n_downsampling):
             mult = 2 ** i
-            model += [nn.Conv2d(ngf*mult, ngf*mult*2, kernel_size=3, stride=2, padding=1, bias=use_bias), norm_layer(ngf*mult*2, affine=True), nn.ReLU(True)]
+            conv_layer = nn.Conv2d(in_channels = args.ngf * mult,
+                                   out_channels = args.ngf * mult * 2,
+                                   kernel_size = 3,
+                                   stride = 2,
+                                   padding = 1,
+                                   bias = use_bias)
+            layers += [conv_layer,
+                       norm_layer(args.ngf * mult * 2, affine=True),
+                       nn.ReLU(True)]
         
-        mult = 2** n_downsampling
+        # Construct Resnet blocks
         for i in range(n_blocks):
-            model += [ResnetBlock(ngf*mult, padding_type, norm_layer, use_dropout, use_bias)]
+            resnet_block = ResnetBlock(dim = args.ngf * mult * 2,
+                                       padding_type = args.padding_type,
+                                       norm_layer = norm_layer,
+                                       use_dropout = args.use_dropout, 
+                                       use_bias = use_bias)
+            layers += [resnet_block]
         
+        # Construct TransposeConv layers
         for i in range(n_downsampling):
             mult = 2**(n_downsampling - i)
-            model += [nn.ConvTranspose2d(ngf*mult, int(ngf*mult/2), kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-                     norm_layer(int(ngf*mult/2)), nn.ReLU(True)]
-        model += [nn.ReflectionPad2d(3)]
-        model += [nn.Conv2d(ngf, ch_out, kernel_size=7, padding=0)]
-        model += [nn.Tanh()]
+            trans_layer = nn.ConvTranspose2d(in_channels = args.ngf*mult,
+                                             out_channels = int(args.ngf*mult/2),
+                                             kernel_size = 3,
+                                             stride = 2,
+                                             padding = 1,
+                                             output_padding = 1,
+                                             bias = use_bias)
+            layers += [trans_layer,
+                      norm_layer(int(args.ngf*mult/2)),
+                      nn.ReLU(True)]
+        layers += [nn.ReflectionPad2d(3)]
+        layers += [nn.Conv2d(args.ngf, args.ch_out, kernel_size=7, padding=0)]
+        layers += [nn.Tanh()]
         
-        self.model = nn.Sequential(*model)
-    
-    def forward(self, x):
-        return self.model(x)
-    
-    
-# PatchGAN D
-class NLayerD(nn.Module):
-    def __init__(self, ch_in, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d, use_sigmoid=False, gpu_ids=[]):
-        super(NLayerD, self).__init__()
-        self.gpu_ids = gpu_ids
-        if norm_layer == nn.InstanceNorm2d:
-            use_bias = True
-        else:
-            use_bias = False
-        kernel_size, padding_size = 4, 1
-        model = [nn.Conv2d(ch_in, ndf, kernel_size, 2, padding_size), nn.LeakyReLU(0.2, True)]
-        
-        nf_mult, nf_mult_prev = 1, 1
-        for n in range(1, n_layers):
-            nf_mult_prev=nf_mult
-            nf_mult = min(2**n, 8)
-            model += [nn.Conv2d(ndf*nf_mult_prev, ndf*nf_mult, kernel_size, 2, padding_size, bias=use_bias), 
-                     norm_layer(ndf*nf_mult), nn.LeakyReLU(0.2,True)]
-        
-        nf_mult_prev, nf_mult = nf_mult, min(2**n_layers, 8)
-        model += [nn.Conv2d(ndf*nf_mult_prev, ndf*nf_mult, kernel_size, 1, padding_size, bias=use_bias), 
-                 norm_layer(ndf*nf_mult), nn.LeakyReLU(0.2, True)]
-        model += [nn.Conv2d(ndf*nf_mult, 1, kernel_size, 1, padding_size)]
-        
-        if use_sigmoid:
-            model += [nn.Sigmoid()]
-        self.model = nn.Sequential(*model)
+        self.model = nn.Sequential(*layers)
     
     def forward(self, x):
         return self.model(x)
     
     
 class PixelD(nn.Module):
-    def __init__(self, ch_in, ndf=64, norm_layer=nn.BatchNorm2d, use_sigmoid=False, gpu_ids=[]):
+    def __init__(self, args, gpu_ids=[]):
         super(PixelD, self).__init__()
+        self.args = args
         self.gpu_ids = gpu_ids
+        
+        norm_layer = get_norm_layer(self.args.norm_type)
         if norm_layer == nn.InstanceNorm2d:
             use_bias = True
         else:
             use_bias = False
-        self.network = [
-            nn.Conv2d(ch_in, ndf, 1, 1, 0), nn.LeakyReLU(0.2, True),
-            nn.Conv2d(ndf, ndf*2, 1, 1, 0, bias=use_bias), norm_layer(ndf*2), nn.LeakyReLU(0.2, True), 
-            nn.Conv2d(ndf*2, 1, 1, 1, 0, bias=use_bias)
-        ]
-        if use_sigmoid:
-            self.network += [nn.Sigmoid()]
-        self.model = nn.Sequential(*self.network)
+            
+        conv_layer1 = nn.Conv2d(in_channels = self.ch_in,
+                                out_channels = self.ndf,
+                                kernel_size=1,
+                                stride=1,
+                                padding=0)
+        conv_layer2 = nn.Conv2d(in_channels = self.ndf,
+                                out_channels = self.ndf*2,
+                                kernel_size=1,
+                                stride=1,
+                                padding=0,
+                                bias = use_bias)
+        conv_layer3 = nn.Conv2d(in_channels = self.ndf*2,
+                                out_channels = 1,
+                                kernel_size=1,
+                                stride=1,
+                                padding=0,
+                                bias=use_bias)
+        
+        layers = [conv_layer1, nn.LeakyReLU(0.2, True),
+                 conv_layer2, norm_layer(args.ndf*2), nn.LeakyReLU(0.2, True),
+                 conv_layer3]
+        if args.use_sigmoid==True:
+            layers += [nn.Sigmoid()]
+
+        self.model = nn.Sequential(*layers)
         
     def forward(self, x):
         return self.model(x)
+    
+    
+class NLayerD(nn.Module):
+    def __init__(self, args, gpu_ids=[], n_layers=3):
+        super(NLayerD, self).__init__()
+        self.args = args
+        self.gpu_ids = gpu_ids
+        
+        norm_layer = get_norm_layer(self.args.norm_type)
+        if norm_layer == nn.InstanceNorm2d:
+            use_bias = True
+        else:
+            use_bias = False
+            
+        conv_layer = nn.Conv2d(in_channels = args.ch_in,
+                               out_channels = args.ndf, 
+                               kernel_size = 4,
+                               stride=2,
+                               padding=1)
+        layers = [conv_layer, nn.LeakyReLU(0.2, True)]
+        
+        nf_mult_prev = 1
+        nf_mult = 1
+        for n in range(1, n_layers):
+            nf_mult_prev = nf_mult
+            nf_mult = min(2**n, 8)
+            conv_layer = nn.Conv2d(in_channels = args.ndf * nf_mult_prev,
+                                   out_channels = args.ndf * nf_mult,
+                                   kernel_size=4,
+                                   stride=2,
+                                   padding=1,
+                                   bias=use_bias)
+            layers += [conv_layer, norm_layer(args.ndf*nf_mult), nn.LeakyReLU(0.2,True)]
+        
+        nf_mult_prev = nf_mult
+        nf_mult = min(2**n_layers, 8)
+        conv_layer = nn.Conv2d(in_channels = args.ndf * nf_mult_prev,
+                               out_channels = args.ndf * nf_mult,
+                               kernel_size=4,
+                               stride=1,
+                               padding=1,
+                               bias=use_bias)
+        final_layer = nn.Conv2d(in_channels = args.ndf * nf_mult,
+                               out_channels = 1,
+                               kernel_size=4,
+                               stride=1,
+                               padding=1)
+        layers += [conv_layer, norm_layer(args.ndf * nf_mult), nn.LeakyReLU(0.2, True), final_layer]
+        if args.use_sigmoid==True:
+            layers += [nn.Sigmoid()]
+        
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.model(x)
+    
